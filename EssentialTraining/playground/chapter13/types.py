3035bf6cb0296938a8d0f97f5f6d951cc657d5f6
YuxinZhang <taylorzhangyx@gmail.com>
2020-03-29 01:04:37 -0700

    fix directory name, add files to chapter13

---
+++ EssentialTraining/playground/chapter13/types.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+x = '47'
+y = int(x)
+z = divmod(y, 3)
+
+print(f'x is {type(x)}')
+print(f'x is {x}')
+print(f'y is {type(y)}')
+print(f'y is {y}')
+print(f'y is {type(z)}')
+print(f'y is {z}')
