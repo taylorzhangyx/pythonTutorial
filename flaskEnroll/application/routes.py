3cc192393475e59f56c913898212ca881a09a01a
YuxinZhang <taylorzhangyx@gmail.com>
2020-04-18 19:44:08 -0700

    add layout and blocks to extend the html based on templates
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- flaskEnroll/application/routes.py
+++ flaskEnroll/application/routes.py
@@ -2,10 +2,15 @@ from application import app
 from flask import render_template
 
 
+user = {}
+user["login"] = False
+user["language"] = 'eng'
+
+
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
-    return render_template('index.html', login=False)
+    return render_template('index.html', **user)
 
 
 @app.route('/classes', methods=['GET'])
